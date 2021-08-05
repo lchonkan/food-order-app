daf5c07f5f413500d655808519462b18fda76d9e
Leonardo Chonkan <leo.ckl@gmail.com>
2021-08-04 23:00:33 -0600

    added a Modal for the Cart using portals

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import classes from './Modal.module.css';
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-const Modal = () => {
-  return <div></div>;
+const BackDrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<BackDrop></BackDrop>, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
 };
 
 export default Modal;
