2d4f3081b0f3700ccb73761296c69d3d4c4d5403
Leonardo Chonkan <leo.ckl@gmail.com>
2021-08-05 20:29:47 -0600

    using the reducer to ADD an item to the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,40 @@
 //this compoment is in charge of managing cart context data and providing the cart context to all components that need access to it.
 
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = props => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
   //these are the handlers for updating the context
-  const addItemToCartHandler = item => {};
-  const removeItemFromCartHandler = id => {};
+  const addItemToCartHandler = item => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+  const removeItemFromCartHandler = id => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   // This is the actual context that gets updated and the initial values
   const cartContext = {
